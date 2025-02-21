35a71f9fe453c501bdd8499dd0d488799ddf5a9f
Eldrige <peldrige8@gmail.com>
2025-02-22 01:50:10 +0200

    add random images

--- controllers/property-controller.js
+++ controllers/property-controller.js
@@ -1,5 +1,18 @@
 import prisma from '../prisma-client.js';
 import catchAsync from '../utils/catch-async.js';
+const PROPERTY_URLS = [
+  'https://res.cloudinary.com/dav5lnlxj/image/upload/v1740180511/house-four_olhjhf.jpg',
+  'https://res.cloudinary.com/dav5lnlxj/image/upload/v1740180496/house-ten_oyi8hl.jpg',
+  'https://res.cloudinary.com/dav5lnlxj/image/upload/v1740180497/house-nine_elmwxn.jpg',
+  'https://res.cloudinary.com/dav5lnlxj/image/upload/v1740180498/house-seven_sdgm91.jpg',
+  'https://res.cloudinary.com/dav5lnlxj/image/upload/v1740180503/house-five_h8y0sz.jpg',
+  'https://res.cloudinary.com/dav5lnlxj/image/upload/v1740180503/house-two_ri9ske.jpg',
+  'https://res.cloudinary.com/dav5lnlxj/image/upload/v1740180507/house-one_ysnzer.jpg',
+  'https://res.cloudinary.com/dav5lnlxj/image/upload/v1740180508/house-six_oganmy.jpg',
+  'https://res.cloudinary.com/dav5lnlxj/image/upload/v1740180508/house-three_hz0tvi.jpg',
+  'https://res.cloudinary.com/dav5lnlxj/image/upload/v1740180509/house-eight_orht91.jpg',
+  'https://res.cloudinary.com/dav5lnlxj/image/upload/v1740180511/house-four_olhjhf.jpg',
+];
 
 export const createProperty = catchAsync(async (req, res) => {
   const { title, description, pricePerNight, location } = req.body;
@@ -54,3 +67,23 @@ export const deleteProperty = catchAsync(async (req, res) => {
   await prisma.property.delete({ where: { id } });
   res.json({ message: 'Property deleted' });
 });
+
+export const assignRandomImages = async (req, res) => {
+  const propertiesWithoutImages = await prisma.property.findMany();
+
+  const updatedProperties = await Promise.all(
+    propertiesWithoutImages.map(async (property) => {
+      const randomImageUrl =
+        PROPERTY_URLS[Math.floor(Math.random() * PROPERTY_URLS.length)];
+
+      return prisma.property.update({
+        where: { id: property.id },
+        data: { image: randomImageUrl },
+      });
+    })
+  );
+
+  res.send(
+    `Updated ${updatedProperties.length} properties with random images.`
+  );
+};
---
+++ prisma/migrations/20250221233747_image/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Property" ADD COLUMN     "image" TEXT NOT NULL DEFAULT 'https://res.cloudinary.com/dav5lnlxj/image/upload/v1740180511/house-four_olhjhf.jpg';
